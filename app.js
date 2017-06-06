e242c009d12b7f5f644bf6edc0170b30fab3406d
unon <unon>
2017-06-05 21:38:55 -0500

    search

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ requirejs.onResourceLoad = function (context, map, depArray) {
 	Vue.use(VueRouter);
 
 	const router = new VueRouter({
-	  mode: (location.origin === "http://localhost:8080" || location.origin.indexOf('surge.sh') != -1) ? 'history' : null,
+	  mode: (/*location.origin === "http://localhost:8080" ||*/ location.origin.indexOf('surge.sh') != -1) ? 'history' : null,
 	  routes: [
 	  	{
 	  		path: '/',
@@ -2159,8 +2159,38 @@ requirejs.onResourceLoad = function (context, map, depArray) {
 
 		var quranSearch = Vue.component('quran-search',{
 			template: '<div>Qur\'aan Search results for: {{ keyword }}\
-							<div v-if="loading"><BR/>Searching...</div>\
-							<div class=text-muted v-else>\
+							<div v-if="loading"><BR/>\
+								<div v-if="loading || (searchResults && searchResults.searching)">\
+								  	<i class="fa fa-spinner fa-pulse"></i>Searching...\
+								</div>\
+							</div>\
+<div class=well>\
+		  <div Xclass=searchResults v-if="searchResults && Object.keys(searchResults).length > 1">\
+			  <!--<HR/>Search results: <BR/>-->\
+			  {{searchResults.matchesCount}} matches found searching for: {{searchResults.keyword}}<BR/><BR/>\
+\
+			  <!-- <div v-for="(matches, key, index) in searchResults"> -->\
+			  <div v-for="result in searchResults.results">\
+			  	{{ result.matches.length }} matches found searching for: {{ result.keyword }} in: {{result.category}} category:\
+\
+				<div class=searchResults v-if="result.matches && result.matches.length" style="overflow:scroll; max-height:200px; white-space: nowrap;" :dir="result.category.indexOf( \'Arabic\' ) != -1 ? \'rtl\' : \'ltr\'">\
+					<div v-for="match in result.matches">\
+						<!-- {{match}} -->\
+						<A href=# v-on:click.stop.prevent=\'goSearchResult(match, result);\' v-bind:title=\'match.verse\'>\
+							{{ match.split(\'|\')[0] }}:{{ match.split(\'|\')[1] }}\
+						</A>\
+						{{ result.category.indexOf( \'Arabic\' ) != -1 ? qUtil.EnToAr( match.split(\'|\')[2] ) : match.split(\'|\')[2] }}\
+					</div>\
+				</div>\
+			  </div>\
+			  <HR/>\
+		  </div>\
+		  <div v-else>Search results shown here when available</div>\
+		  <h6><small><span v-cloak v-if=message class=text-muted>\
+		  	{{message}}\
+		  </span></small></h6>\
+		</div>\
+							<div class=text-muted v-if="!loading">\
 								{{searchResults}} <BR/><BR/> {{data}}\
 							</div>\
 					   </div>',
@@ -2172,6 +2202,7 @@ requirejs.onResourceLoad = function (context, map, depArray) {
 					data: null,
 					message: null,
 					searchResults: null,
+					qUtil: {},
 				}
 			},
 			created: function() {
@@ -2192,9 +2223,10 @@ requirejs.onResourceLoad = function (context, map, depArray) {
 		    		this.loading = true;
 		    		this.data = null;
 		    		var comp = this, startTime = new Date(), endTime;
-	    			require(['Q', 'qSearch', 'underscore'], function(Q, qSearch, _){
-	    				qSearch.searchAsync( comp.keyword ).then(function(data){
-	    					comp.data = data;
+	    			require(['Q', 'qUtil', 'qSearch', 'underscore'], function(Q, qUtil, qSearch, _){
+	    				qSearch.searchAsync( comp.keyword ).then(function(results){
+	    					comp.qUtil = qUtil;
+	    					comp.data = results;
 	    					comp.loading = false;
 	    					comp.error = null;
 							comp.searchResults = results || {};
@@ -2217,6 +2249,9 @@ requirejs.onResourceLoad = function (context, map, depArray) {
 							}, 200);
 	    				});//TODO: add error handling
 	    			});
+		    	},
+		    	goSearchResult: function(match, result){
+		    		console.log(match, result);
 		    	},
 		    }
 		});
